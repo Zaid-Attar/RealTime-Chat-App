df6019a6523d6cc07b0f797decc751bcef504cf4
Zaid Attar <zaidattar2006@gmail.com>
2026-07-14 21:46:05 +0530

    restart

--- frontend/src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-//const BASE_URL = import.meta.env.MODE == "development" ? "http://localhost:5090/api" : "/api"
-const api = axios.create({
-    baseURL : "/api",
-});
-
-export default api;
--- frontend/src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatDate(date) {
-  return date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-}
